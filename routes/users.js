1b4dd554a1670ae0dbfdcfe11112694914b59b11
ssszz@ubantu <spachitralada@gmail.com>
2023-01-31 16:49:57 +0700

    show all user complete

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,3 @@
-const { request, json } = require('express');
 var express = require('express');
 var router = express.Router();
 const user = require('../models/users_adaptor.js');
@@ -9,46 +8,45 @@ router.get('/', function (req, res, next) {
 });
 
 // /users/all  method get  must sent with bodyjason {token:ssszzสadmin}
-// ถ้า deploy ต้องเอาออก
-router.get('/all', (req, res, err)=>{
-    if (err){
-        console.log("can not get all user : internal server err");
-        return next(err);
-    }
-    if (req.body.token == "ssszzสadmin"){
-        user.find({},(err,post)=>{
-            if (err){
-                console.log("require token to get all data");
-                return next(err);
-            }
-            res.json(post);
-            console.log("all user data have been get");
-        });
-    }
+// danger  นี่คือสิทธิของ
+router.get('/all', (req, res, next) => {
+    user.find({}, (err, post) => {
+        if (err){
+            console.log("canot get all data");
+            return next(err);
+        }
+        if (req.body.token != "โkey") {
+            console.log("unauthorize atemp to get all data !!!!!");
+            return res.send("require token to get all data");
+        }
+        res.json(post);
+        console.log("all user data have been get");
+    });
+
 });
 ////////////////////////////////////////////////////////////
 
-router.get('/login',(req,res,next)=>{
-    user.findOne(request.body,(err,post)=>{
-        if (err){
+router.get('/login', (req, res, next) => {
+    user.findOne(req.body, (err, post) => {
+        if (err) {
             console.log("can not login : internal server err");
             return next(err);
         }
-        if (json(post).length > 0){
-            console.log(request.body); //print check
+        if (json(post).length > 0) {
+            console.log(req.body); //print check
             console.log("logged in");//print check
-            res.json({"success":true});
-        }else{
-            console.log(request.body);
+            res.json({ "success": true });
+        } else {
+            console.log(req.body);
             console.log("invalid username or password");
-            res.json({"success":false});
+            res.json({ "success": false });
         }
     });
 });
 
 router.post('/create', (req, res, next) => {
-    user.create(req.body, (err,post)=>{
-        if(err){
+    user.create(req.body, (err, post) => {
+        if (err) {
             console.log("can not create user");
             return next(err);
         }
