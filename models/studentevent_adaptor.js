eb253e9ddd208ded9e7d52275c0d235fac7fbfd1
ssszz@ubantu <spachitralada@gmail.com>
2023-02-06 14:08:23 +0700

    implement studentevent adapter

---
+++ models/studentevent_adaptor.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const url = 'mongodb://127.0.0.1:27017/event_alerter';
+mongoose.connect(url);
+
+// ตรวจสอบการเชื่อมต่อ
+const db = mongoose.connection;
+db.once('open', () => {
+    console.log('DB connected ^_^ :', url);
+})
+
+db.on('error', (err) => {
+    console.error('connection error T_T :', err);
+})
+
+const eventSchema = new mongoose.Schema({//collection record ของนักศึกษา... ทำกิจกรรม... เเล้ว เพื่อให้ง่ายต่อการ searching
+    stdid:String,
+    eventid:String,
+    create_time: { type: Date, default: Date().toLocaleString("th-TH") }
+})
+
+module.exports = mongoose.model('event', eventSchema);
+//ตัวสร้าง schema ไว้เชื่อต่อ database
